e48ecb7cf834312279d64cc069793a77dc789621
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2024-04-02 22:36:08 +0300

    Update main.py
    Pievienotas funkcijas - jaunas spēles uzsākšanai, uzvarētāja noteikšanai.

--- app/main.py
+++ app/main.py
@@ -9,13 +9,31 @@ def main():
     current_turn = [1]  # Uzglabā informāciju par pašreizējo spēlētāju iekš saraksta, lai būtu iespējama modificēšana
     player_points = [80, 80]  # Sākotnējie punkti abiem spēlētājiem
 
+    #Funkcija jaunas spēles uzsākšanai, visu elementu atjaunošana uz sākuma stāvokli
+    def new_game():
+        global numbers_list
+        numbers_list = []
+        ok_button.pack()   
+        player_points = [80, 80]  
+        winner_label.config(text="Uzvarētājs:")  
+        num_input.config(state="normal")  
+        player_input.config(state="disabled")  
+        numbers_label.config(text="")  
+        player1_points_label.config(text="Spēlētājs 1 punkti: 80")  
+        player2_points_label.config(text="Spēlētājs 2 punkti: 80")
+        new_game_button.pack_forget()
+
     def update_points():
         # Tā kā current_turn tagad ir saraksts, mēs izmantojam current_turn[0]
         num = int(player_input.get())
         if num in [1, 2, 3]:
             player_points[current_turn[0] - 1] -= num
+            global numbers_list
+            numbers_list.remove(num)
+            numbers_label.config(text = str(numbers_list))
             player1_points_label.config(text=f"Spēlētājs 1 punkti: {player_points[0]}")
             player2_points_label.config(text=f"Spēlētājs 2 punkti: {player_points[1]}")
+            winner_check()
             
             # Pārslēdzam kārtu uz nākamo spēlētāju
             current_turn[0] = 2 if current_turn[0] == 1 else 1
@@ -25,6 +43,21 @@ def main():
             player_input.delete(0, tk.END)
         else:
             turn_label.config(text="Ievadiet skaitli: 1, 2 vai 3")
+
+    #Funkcija, kas nosaka uzvarētāju
+    def winner_check():
+        global numbers_list
+        if not numbers_list:
+            winner = None
+            if player_points[0] > player_points[1]:
+                winner = "Spēlētājs 1"
+            elif player_points[0] < player_points[1]:
+                winner = "Spēlētājs 2"
+            else:
+                winner = "Neizšķirts"
+        
+            winner_label.config(text=f"Uzvarētājs: {winner}")
+            new_game_button.pack() 
 
 
     
@@ -39,6 +72,7 @@ def main():
         try:
             length = int(num_input.get())
             if 15 <= length <= 25:
+                global numbers_list
                 numbers_list = [random.randint(1, 3) for _ in range(length)]
                 numbers_label.config(text=str(numbers_list))
                 player1_points_label.config(text="Spēlētājs 1 punkti: 80")
@@ -46,6 +80,8 @@ def main():
                 turn_label.config(text="Spēlētāja kārta: 1")
                 winner_label.config(text="Uzvarētājs: ")
                 player_input.config(state="normal")
+                ok_button.pack_forget() 
+                num_input.config(state="disabled")  
             else:
                 numbers_label.config(text="Garumam jābūt diapazonā no 15 līdz 25")
         except ValueError:
@@ -76,6 +112,9 @@ def main():
 
     winner_label = tk.Label(window, text="Uzvarētājs")
     winner_label.pack(pady=20)
+
+    new_game_button = tk.Button(window, text="Spēlēt vēlreiz!", command=new_game)
+    move_button.pack()
 
     tk.mainloop()
 
